1d9bf97153b6737f98febae1b106164ec1d44194
PaidActorDe <160431967+PaidActorDe@users.noreply.github.com>
2025-04-25 17:00:20 +0200

    Update scp_407_register.lua

--- scp_407/lua/autorun/scp_407_register.lua
+++ scp_407/lua/autorun/scp_407_register.lua
@@ -1,6 +1,6 @@
 list.Set("SpawnableEntities", "ent_scp_407", {
     PrintName = "SCP-407",
-    ClassName = "ent_scp_407t",
+    ClassName = "ent_scp_407",
     Category = "SCP",
     Model = "models/props/cs_office/radio.mdl"
 })
