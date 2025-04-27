c2288ae9c72ef0bcc968a6b131f944dc7130835f
Kevin Jahreiß <kevin.jahreiss@gmx.de>
2025-04-27 11:28:07 +0200

    Icon

---
+++ scp_113/lua/autorun/scp_113_register.lua
@@ -0,0 +1,6 @@
+list.Set("SpawnableEntities", "ent_scp_113", {
+    PrintName = "SCP-113",
+    ClassName = "ent_scp_113",
+    Category = "SCP",
+    Model = "models/props_junk/watermelon01.mdl"
+})
